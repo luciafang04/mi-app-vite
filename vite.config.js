95742d1ceb5ccbcb1750b0901f4fa09a20886bd7
Luciafang04 <luciafangher2004@gmail.com>
2026-02-03 09:38:33 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/mi-app-vite/',
   server: {
     allowedHosts: ["subatomic-tangela-liquidly.ngrok-free.dev"],
   },
