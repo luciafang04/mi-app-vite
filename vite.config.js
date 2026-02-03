55957aefe27fbec7ca06fc7fab667ff6480b8d6c
Luciafang04 <luciafangher2004@gmail.com>
2026-02-03 10:06:50 +0100

    Cloudflare

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mi-app-vite/',
-  server: {
-    allowedHosts: ["subatomic-tangela-liquidly.ngrok-free.dev"],
-  },
-});
+  base: '/'   // <- importante para Cloudflare Pages
+})
